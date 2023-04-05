c427febad1ba7d12067b1dbaf5724e8dbb534fd9
ABEL K ANTONY <abelkantony98@gmail.com>
2023-04-05 10:04:20 +0000

    UPDATE

--- .ccls-cache/@home@runner@Linked-list/main.cpp
+++ .ccls-cache/@home@runner@Linked-list/main.cpp
@@ -10,10 +10,15 @@ struct Node
 struct Node *Head = NULL ;
 void insertData(int value)
 {
+	Node *new_node = (struct Node*)malloc(sizeof(struct Node));
+	new_node->data = value;
+	new_node->next = NULL;
+	
 	if (Head->next == NULL)
 	{
 		Node *new_node = (struct Node*)malloc(sizeof(struct Node));
 		Head->next = new_node;
+	
 		
 	}
 }
